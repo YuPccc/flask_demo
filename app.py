b5fc697ab466bc8749c2ade405c099bde866d962
Paul Yu <pyu4203@gmail.com>
2023-07-15 14:43:18 +0800

    add index page

--- app.py
+++ app.py
@@ -1,16 +1,32 @@
 
-from flask import Flask, request
+from flask import Flask, request, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, chain!'
+@app.route('/index')
+@app.route('/home')
+def index():
+    return render_template('index.html', name=name, movies=movies)
+
 
 @app.route('/book/list')
 def get_book():
     page=request.args.get("page", default=1, type=int)
     return f'{page}'
 
+name = 'Grey Li'
+movies = [
+    {'title': 'My Neighbor Totoro', 'year': '1988'},
+    {'title': 'Dead Poets Society', 'year': '1989'},
+    {'title': 'A Perfect World', 'year': '1993'},
+    {'title': 'Leon', 'year': '1994'},
+    {'title': 'Mahjong', 'year': '1996'},
+    {'title': 'Swallowtail Butterfly', 'year': '1996'},
+    {'title': 'King of Comedy', 'year': '1999'},
+    {'title': 'Devils on the Doorstep', 'year': '1999'},
+    {'title': 'WALL-E', 'year': '2008'},
+    {'title': 'The Pork of Music', 'year': '2012'},
+]
 
 if __name__ == '__main__':
     app.run( debug=True)
